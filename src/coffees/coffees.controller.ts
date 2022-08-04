120079b66ed51db2ab45ece561fd103998e7e082
JonatasCopernico <jonatas.pimenta@copernico.com.br>
2022-08-04 13:12:00 -0300

    :sparkles: adds route params

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
   findAll(): string {
     return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): string {
+    return `This action returns a #${id} coffee`;
   }
 }
